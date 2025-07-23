32ab21e843e88bd5064f6346ddf5750d4dbf845f
AbdulJabbar13 <abduljabbar@gmail.com>
2025-07-23 09:41:26 +0530

    first commit for demo purpose

---
+++ Instagram1/src/main/java/Tcs/org/Instagram1/App.java
@@ -0,0 +1,7 @@
+package Tcs.org.Instagram1;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ Instagram1/src/test/java/Tcs/org/Instagram1/AppTest.java
@@ -0,0 +1,13 @@
+package Tcs.org.Instagram1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
